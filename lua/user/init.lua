42ff954c6289f5b5c17d010061e4d939d9acba30
Tobias Hunger <tobias.hunger@slint-ui.com>
2022-04-28 23:15:13 +0200

    Simplify rust-tools setup (now that this is possible;-)

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -81,6 +81,24 @@ local config = {
           "simrat39/rust-tools.nvim",
           requires = { "nvim-lspconfig", "nvim-lsp-installer", "nvim-dap", "Comment.nvim", "plenary.nvim" },
           -- Is configured via the server_registration_override installed below!
+          config = function()
+            local extension_path = vim.fn.stdpath "data" .. "/dapinstall/codelldb/extension"
+            local codelldb_path = extension_path .. "/adapter/codelldb"
+            local liblldb_path = extension_path .. "/lldb/lib/liblldb.so"
+
+            require("rust-tools").setup {
+              server = server_opts,
+              -- dap = {
+              --   adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+              -- },
+              tools = {
+                inlay_hints = {
+                  parameter_hints_prefix = "  ",
+                  other_hints_prefix = "  ",
+                },
+              },
+            }
+          end,
         },
         {
           "Saecki/crates.nvim",
@@ -234,32 +252,6 @@ local config = {
     treesitter = {
       ensure_installed = {},
     },
-  },
-
-  lsp = {
-    server_registration = function(server, server_opts)
-      -- Special code for rust.tools.nvim!
-      if server.name == "rust_analyzer" then
-        local extension_path = vim.fn.stdpath "data" .. "/dapinstall/codelldb/extension"
-        local codelldb_path = extension_path .. "/adapter/codelldb"
-        local liblldb_path = extension_path .. "/lldb/lib/liblldb.so"
-
-        require("rust-tools").setup {
-          server = server_opts,
-          -- dap = {
-          --   adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-          -- },
-          tools = {
-            inlay_hints = {
-              parameter_hints_prefix = "  ",
-              other_hints_prefix = "  ",
-            },
-          },
-        }
-      else
-        server:setup(server_opts)
-      end
-    end,
   },
 
   -- Disable default plugins
